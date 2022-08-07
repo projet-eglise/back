d661ff6557ec1629a942331c324036abe529a3c9
projet-eglise <projet-eglise@protonmail.com>
2022-08-07 17:32:13 +0200

    Next

--- app/src/Infrastructure/Authentication/Controllers/AdminLoginController.php
+++ app/src/Infrastructure/Authentication/Controllers/AdminLoginController.php
@@ -4,25 +4,20 @@ declare(strict_types=1);
 
 namespace Src\Infrastructure\Authentication\Controllers;
 
-use Exception;
 use Illuminate\Http\Request;
 use Src\Application\Authentication\CheckCredentials;
 use Src\Application\Authentication\CheckIfIsAdmin;
 use Src\Domain\Authentication\JwtToken;
 use Src\Domain\Authentication\Password;
 use Src\Infrastructure\Shared\Interfaces\Controller;
-use Src\Infrastructure\Authentication\Repositories\EloquentUserRepository;
 use Src\Domain\Shared\Email;
 
 final class AdminLoginController implements Controller
 {
-    private CheckCredentials $CheckCredentials;
-    private CheckIfIsAdmin $CheckIfIsAdmin;
-
-    public function __construct(private EloquentUserRepository $repository)
-    {
-        $this->CheckCredentials = new CheckCredentials($repository);
-        $this->CheckIfIsAdmin = new CheckIfIsAdmin($repository);
+    public function __construct(
+        private CheckCredentials $CheckCredentials,
+        private CheckIfIsAdmin $CheckIfIsAdmin
+    ) {
     }
 
     public function render(Request $request)
